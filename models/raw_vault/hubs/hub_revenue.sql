648e623404baf7ba1406e2b03bb49ed501063aaa
sheetal16gowda@gmail.com <sheetal16gowda@gmail.com>
2023-06-14 04:19:53 +0000

    changes added

---
+++ models/raw_vault/hubs/hub_revenue.sql
@@ -0,0 +1,8 @@
+{%- set source_model = "v_stg_revenue"   -%}
+{%- set src_pk = "REVENUE_PK"          -%}
+{%- set src_nk = "REVENUE_HK"         -%}
+{%- set src_ldts = "LDTS"      -%}
+{%- set src_source = "RECORD_SOURCE"    -%}
+
+{{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
+                src_source=src_source, source_model=source_model) }}
